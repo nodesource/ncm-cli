de6700b2570e5229f26e2f0818d23f6e0590bb50
michaelsterpka <michaelsterpka@gmail.com>
2019-02-12 18:09:35 -0800

    command ref changes: - ncm-cli details <pkg-name>@<ver> (fails when no args are passed) - ncm-cli report <directory> (fails when invalid dir is passed i.e. no package.json) - small fixes to reporting, mostly color mapping

--- commands/details.js
+++ commands/details.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const analyze = require('../lib/ncm-analyze-tree')
 const { getTokens } = require('../lib/config')
 const {
   formatAPIURL,
@@ -11,29 +10,18 @@ const {
   outputReport,
   reportFailMsg
 } = require('../lib/report/util')
-const longReport = require('../lib/report/long')
-const shortReport = require('../lib/report/short')
 const moduleReport = require('../lib/report/module')
 const logger = require('../lib/logger')
 const { helpHeader } = require('../lib/help')
 const semver = require('semver')
 const L = console.log
 
-const SEVERITY_MAP = {
-  NONE: 0,
-  LOW: 1,
-  MEDIUM: 2,
-  HIGH: 3,
-  CRITICAL: 4
-}
+module.exports = details
 
-module.exports = verify
-
-async function verify (argv, _dir) {
+async function details (argv, _dir) {
   const {
     json,
-    output,
-    long
+    output
   } = argv
   let { dir = _dir } = argv
   if (!dir) dir = process.cwd()
@@ -47,8 +35,21 @@ async function verify (argv, _dir) {
 
   /* details */
   if (argv._.length > 1) {
-    let pkgName = argv._.length === 4 ? argv._[1] : argv._[1].split('@')[0]
-    let pkgVer = argv._.length === 4 ? argv._[3] : argv._[1].split('@')[1]
+    let pkgName, pkgVer
+    switch (argv._.length) {
+      case 4:
+        pkgName = argv._[1]
+        pkgVer = argv._[3]
+        break
+      case 3:
+        pkgName = argv._[1]
+        pkgVer = argv._[2]
+        break
+      case 2:
+        pkgName = argv._[1].split('@')[0]
+        pkgVer = argv._[1].split('@')[1]
+        break
+    }
 
     if (!pkgName || !semver.valid(pkgVer) || argv.length > 4) {
       L()
@@ -143,69 +144,11 @@ async function verify (argv, _dir) {
     if (json) jsonReport(report)
     if (output) outputReport(report, output)
     if (hasFailures) process.exitCode = 1
-  }
-
-  /* verify */
-  if (argv._.length === 1) {
-    const pkgScores = []
-    let hasFailures = false
-
-    let data
-    try {
-      data = await analyze({
-        dir,
-        token: session,
-        url: formatAPIURL('/ncm2/api/v2/graphql')
-      })
-    } catch (err) {
-      process.exitCode = 1
-      if (err.code === 'ENOENT') {
-        L()
-        reportFailMsg(`Unable to find project at: ${dir}`)
-        L()
-      } else {
-        L()
-        reportFailMsg('Unable to fetch project report.')
-        L()
-      }
-      return
-    }
-
-    for (const { name, version, scores } of data) {
-      let maxSeverity = SEVERITY_MAP.NONE
-      let license
-      const failures = []
-
-      for (const score of scores) {
-        const severityValue = SEVERITY_MAP[score.severity]
-
-        if (score.group !== 'compliance' &&
-            score.group !== 'security' &&
-            score.group !== 'risk') {
-          continue
-        }
-
-        if (severityValue > maxSeverity) {
-          maxSeverity = severityValue
-        }
-
-        if (score.pass === false) {
-          failures.push(score)
-          hasFailures = true
-        }
-
-        if (score.name === 'license') {
-          license = score
-        }
-      }
-      pkgScores.push({ name, version, maxSeverity, failures, license, scores })
-    }
-
-    if (!json && !output && !long) shortReport(pkgScores, dir)
-    if (long) longReport(pkgScores, dir)
-    if (json) jsonReport(pkgScores)
-    if (output) outputReport(pkgScores, output)
-    if (hasFailures) process.exitCode = 1
+  } else {
+    process.exitCode = 1
+    L()
+    reportFailMsg('Unable to fetch details -- please specify a package.')
+    L()
   }
 }
 
