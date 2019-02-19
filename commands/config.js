0077a165f1b79d406fafe13d88112270eb10e79d
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2019-02-18 17:23:59 -0800

    refactor: remove logger() and handleError()
    Yay old patterns finally begone!
    
    Finally resolves
    https://github.com/nodesource/ncm-cli/issues/36
    
    Also rewrites some of the `config` command.
    - Bad input checking has been fixed.
    - reset has a prompt.
    - list attempts to hide tokens unless --unhide is provided.

--- commands/config.js
+++ commands/config.js
@@ -4,28 +4,31 @@ const {
   getValue,
   setValue,
   delValue,
-  resetState
+  resetState,
+  keyNames
 } = require('../lib/config')
-const logger = require('../lib/logger')
-const { handleError } = require('../lib/util')
+const { queryReadline } = require('../lib/util')
 const { helpHeader } = require('../lib/help')
-const { COLORS } = require('../lib/ncm-style')
+const {
+  COLORS,
+  failure,
+  success,
+  tooltip,
+  box,
+  formatError
+} = require('../lib/ncm-style')
 const chalk = require('chalk')
 const L = console.log
+const E = console.error
 
 module.exports = config
 module.exports.optionsList = optionsList
-
-const userAccess = [
-  'token',
-  'org'
-]
 
 async function config (argv, action, key, value) {
   if (argv.help) {
     printHelp()
     return
-  } else if (!action || !key || !value) {
+  } else if (!action) {
     printHelp()
     process.exitCode = 1
     return
@@ -33,53 +36,95 @@ async function config (argv, action, key, value) {
 
   switch (action) {
     case 'set':
-      let setErr = setValue(key, value)
-      if (setErr) {
-        handleError(setErr)
-      } else {
-        logger([{ text: 'NCM-CLI', style: 'ncm' }])
-        logger([{ text: `<${key}> `, style: 'success' }, { text: `has been set to:`, style: [] }])
-        logger([{ text: `${value}`, style: [] }])
-        logger()
+      if (!key || !value) {
+        printHelp()
+        process.exitCode = 1
+        return
       }
+      try {
+        setValue(key, value)
+      } catch (err) {
+        E()
+        E(formatError(`Attempted to set invalid config key "${key}"`, err))
+        E()
+      }
+      L(success(`<${key}> has been set to:`))
+      L(value)
+      L()
       break
+
     case 'get':
-      let { err, val } = getValue(key)
-      if (err) {
-        handleError(err)
-      } else {
-        logger([{ text: 'NCM-CLI', style: 'ncm' }])
-        logger([{ text: `<${key}>:`, style: 'success' }])
-        logger([{ text: `${val || ''}`, style: [] }])
-        logger()
+      if (!key) {
+        printHelp()
+        process.exitCode = 1
+        return
       }
+      let val
+      try {
+        val = getValue(key)
+      } catch (err) {
+        E()
+        E(formatError(`Attempted to get invalid config key "${key}"`, err))
+        E()
+      }
+      L(success(`<${key}> is:`))
+      L(val)
+      L()
       break
+
     case 'del':
-      let delErr = delValue(key)
-      if (delErr) {
-        handleError(delErr)
-      } else {
-        logger([{ text: 'NCM-CLI', style: 'ncm' }])
-        logger([{ text: `<${key}>:`, style: 'success' }])
-        logger([{ text: `${value || ''}`, style: [] }])
-        logger()
+      if (!key) {
+        printHelp()
+        process.exitCode = 1
+        return
       }
+      try {
+        delValue(key)
+      } catch (err) {
+        E()
+        E(formatError(`Attempted to delete invalid config key "${key}"`, err))
+        E()
+      }
+      L(success(`<${key}> was deleted`))
+      L()
       break
+
     case 'reset':
       resetState()
 
-      logger([{ text: 'NCM-CLI', style: 'ncm' }])
-      logger([{ text: `Config reset successful.`, style: 'success' }])
-      logger()
-      break
-    case 'list':
-      for (let key of userAccess) {
-        let { err, val } = getValue(key)
-        if (err) continue
-        logger([{ text: `<${key}> `, style: 'success' }, { text: `${val}`, style: [] }])
+      L(box('!', 'Are you sure you want to reset all config? (y/N)', COLORS.orange))
+
+      const answer = (await queryReadline(chalk`{${COLORS.light1} > }`)).trim()
+      if (answer.toLowerCase() !== 'y') {
+        L(failure('Did not reset any config.'))
+        L()
+        process.exitCode = 1
+        return
       }
-      logger()
+      L(success(`All config was reset.`))
+      L()
       break
+
+    case 'list':
+      const { unhide } = argv
+
+      for (let key of keyNames) {
+        let val = getValue(key)
+
+        if (val.trim() === '') {
+          val = chalk`{italic {${COLORS.light1} (empty)}}`
+        } else if (/token/i.test(key) && !unhide) {
+          val = chalk`{italic {${COLORS.light1} (${val.length} characters hidden)}}`
+        }
+        L(chalk`{${COLORS.teal} <${key}>} ${val}`)
+      }
+      if (!unhide) {
+        L()
+        L(tooltip('Run with --unhide to show hidden values.'))
+      }
+      L()
+      break
+
     default:
       printHelp()
       process.exitCode = 1
