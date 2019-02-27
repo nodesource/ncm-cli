1f1f304595fcb53577f876b8813a53fc347bca45
raynos <raynos2@gmail.com>
2019-02-27 18:37:02 +0100

    commands/report: document security flag

--- commands/report.js
+++ commands/report.js
@@ -127,6 +127,7 @@ function optionsList () {
 {${COLORS.light1} ncm} {${COLORS.yellow} report} {${COLORS.teal} <directory>}
   {${COLORS.teal} -l, --long}              {white Expanded output with module list}
   {${COLORS.teal} --compliance}            {white Expanded output with compliance failures}
+  {${COLORS.teal} --security}              {white Expanded output with security failures}
   {${COLORS.teal} -j, --json}              {white Output report as JSON}
   {${COLORS.teal} -o, --output <filepath>} {white Write JSON report to file}
   `.trim()
