3d858b79efc6ce13d8f3e5e13fb3509e73ee42a4
Jake Verbaten <raynos2@gmail.com>
2019-03-04 16:31:20 +0100

    Implement filter logic for types and severity level (#82)

--- test/report.js
+++ test/report.js
@@ -15,13 +15,13 @@ test('report output matches snapshot', (t) =>
     t.notOk(stderr)
     t.matchSnapshot(stdout, 'report-output')
     t.ok(/mock-project Report/.test(stdout))
-    t.ok(/32 .+packages checked/.test(stdout))
-    t.ok(/chalk @ 2.4.2/.test(stdout))
+    t.ok(/37 .+packages checked/.test(stdout))
+    t.ok(/handlebars @ 4.0.5/.test(stdout))
     t.notOk(/has-flag @ 3.0.0/.test(stdout))
 
     const out = stdout.toString()
-    t.ok(/1 noncompliant modules found/.test(out))
-    t.ok(/1 security vulnerabilities found/.test(out))
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
     t.end()
   })
 )
@@ -35,10 +35,12 @@ test('report --compliance output', (t) =>
     t.matchSnapshot(stdout, 'report-output-compliance')
 
     const out = stdout.toString()
-    t.ok(/1 noncompliant modules found/.test(out))
+    t.ok(/2 noncompliant modules found/.test(out))
     t.ok(/left-pad @ 1.3.0/.test(out))
+    t.ok(/ms @ 0.7.1/.test(out))
     t.ok(/WTFPL/.test(out))
-    t.ok(/1 security vulnerabilities found/.test(out))
+    t.ok(/UNKNOWN/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
     t.end()
   })
 )
@@ -52,13 +54,34 @@ test('report -c output', (t) =>
     t.matchSnapshot(stdout, 'report-output-compliance')
 
     const out = stdout.toString()
-    t.ok(/1 noncompliant modules found/.test(out))
+    t.ok(/2 noncompliant modules found/.test(out))
     t.ok(/left-pad @ 1.3.0/.test(out))
     t.ok(/WTFPL/.test(out))
-    t.ok(/1 security vulnerabilities found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
     t.end()
   })
 )
+
+test('report --filter=compliance output', (t) => {
+  const cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    '--filter=compliance --color=16m'
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-compliance')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/left-pad @ 1.3.0/.test(out))
+    t.ok(/ms @ 0.7.1/.test(out))
+    t.ok(/WTFPL/.test(out))
+    t.ok(/UNKNOWN/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.end()
+  })
+})
 
 test('report --security output', (t) =>
   exec(`node ${NCM_BIN} report ${MOCK_PROJECT} --security --color=16m`, {
@@ -69,10 +92,15 @@ test('report --security output', (t) =>
     t.matchSnapshot(stdout, 'report-output-security')
 
     const out = stdout.toString()
-    t.ok(/1 noncompliant modules found/.test(out))
-    t.ok(/1 security vulnerabilities found/.test(out))
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
     t.ok(/handlebars @ 4.0.5/.test(out))
+    t.ok(/ms @ 0.7.1/.test(out))
+    t.ok(/brace-expansion @ 1.1.2/.test(out))
+    t.ok(/debug @ 2.2.0/.test(out))
     t.ok(/1H/.test(out))
+    t.ok(/1M/.test(out))
+    t.ok(/1L/.test(out))
     t.end()
   })
 )
@@ -86,13 +114,227 @@ test('report -s output', (t) =>
     t.matchSnapshot(stdout, 'report-output-security')
 
     const out = stdout.toString()
-    t.ok(/1 noncompliant modules found/.test(out))
-    t.ok(/1 security vulnerabilities found/.test(out))
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
     t.ok(/handlebars @ 4.0.5/.test(out))
     t.ok(/1H/.test(out))
     t.end()
   })
 )
+
+test('report --filter=security output', (t) =>
+  exec(`node ${NCM_BIN} report ${MOCK_PROJECT} --filter=security --color=16m`, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.ok(/ms @ 0.7.1/.test(out))
+    t.ok(/brace-expansion @ 1.1.2/.test(out))
+    t.ok(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.ok(/1M/.test(out))
+    t.ok(/1L/.test(out))
+    t.end()
+  })
+)
+
+test('report --filter=high --security output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=high --security --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-high-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.notOk(/ms @ 0.7.1/.test(out))
+    t.notOk(/brace-expansion @ 1.1.2/.test(out))
+    t.notOk(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.notOk(/1M/.test(out))
+    t.notOk(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=high output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=high --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-high-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.notOk(/ms @ 0.7.1/.test(out))
+    t.notOk(/brace-expansion @ 1.1.2/.test(out))
+    t.notOk(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.notOk(/1M/.test(out))
+    t.notOk(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=h output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=h --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-high-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.notOk(/ms @ 0.7.1/.test(out))
+    t.notOk(/brace-expansion @ 1.1.2/.test(out))
+    t.notOk(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.notOk(/1M/.test(out))
+    t.notOk(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=high,security output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=high,security --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-high-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.notOk(/ms @ 0.7.1/.test(out))
+    t.notOk(/brace-expansion @ 1.1.2/.test(out))
+    t.notOk(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.notOk(/1M/.test(out))
+    t.notOk(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=medium --security output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=medium --security --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-med-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.notOk(/ms @ 0.7.1/.test(out))
+    t.ok(/brace-expansion @ 1.1.2/.test(out))
+    t.notOk(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.ok(/1M/.test(out))
+    t.notOk(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=m --security output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=m --security --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-med-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.notOk(/ms @ 0.7.1/.test(out))
+    t.ok(/brace-expansion @ 1.1.2/.test(out))
+    t.notOk(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.ok(/1M/.test(out))
+    t.notOk(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=low --security output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=low --security --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-med-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.ok(/ms @ 0.7.1/.test(out))
+    t.ok(/brace-expansion @ 1.1.2/.test(out))
+    t.ok(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.ok(/1M/.test(out))
+    t.ok(/1L/.test(out))
+    t.end()
+  })
+})
+
+test('report --filter=l --security output', (t) => {
+  let cmd = `node ${NCM_BIN} report ${MOCK_PROJECT} ` +
+    `--filter=l --security --color=16m`
+  exec(cmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, (err, stdout, stderr) => {
+    t.equal(err.code, 1)
+    t.notOk(stderr)
+    t.matchSnapshot(stdout, 'report-output-med-security')
+
+    const out = stdout.toString()
+    t.ok(/2 noncompliant modules found/.test(out))
+    t.ok(/4 security vulnerabilities found/.test(out))
+    t.ok(/handlebars @ 4.0.5/.test(out))
+    t.ok(/ms @ 0.7.1/.test(out))
+    t.ok(/brace-expansion @ 1.1.2/.test(out))
+    t.ok(/debug @ 2.2.0/.test(out))
+    t.ok(/1H/.test(out))
+    t.ok(/1M/.test(out))
+    t.ok(/1L/.test(out))
+    t.end()
+  })
+})
 
 test('report output matches snapshot', (t) =>
   exec(`node ${NCM_BIN} report --long ${MOCK_PROJECT} --color=16m`, {
