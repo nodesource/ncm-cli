2e1789442a4f31e9de81b2db78f278340ae43af4
Jake Verbaten <raynos2@gmail.com>
2019-03-11 17:41:41 +0100

    Graphql mock test (#99)

--- test/details.js
+++ test/details.js
@@ -1,21 +1,16 @@
 'use strict'
 
-const { exec } = require('child_process')
-const path = require('path')
-const { test } = require('tap')
+const NCMTestRunner = require('./lib/test-runner.js')
 
-const NCM_BIN = path.join(__dirname, '..', 'bin', 'ncm-cli.js')
-
-test('details output matches snapshot', (t) =>
-  exec(`node ${NCM_BIN} details npm@6.8.0 --color=16m`, {
-    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
-  }, (err, stdout, stderr) => {
+NCMTestRunner.test('details output matches snapshot', (runner, t) => {
+  runner.exec('details npm@6.8.0', (err, stdout, stderr) => {
     t.equal(err.code, 1)
     t.notOk(stderr)
     t.matchSnapshot(stdout, 'details-output')
     t.ok(/npm @ 6.8.0/.test(stdout))
     t.ok(/No Security Vulnerabilities/.test(stdout))
     t.ok(/Noncompliant license: Artistic-2.0/.test(stdout))
+
     t.end()
   })
-)
+})
---
+++ test/lib/test-runner.js
@@ -0,0 +1,134 @@
+const { test } = require('tap')
+const tapeCluster = require('tape-cluster')
+const graphql = require('graphql')
+const express = require('express')
+const expressGraphql = require('express-graphql')
+const path = require('path')
+const exec = require('child_process').exec
+
+const mockPackages = require('./mock-packages.js')
+const NCM_BIN = path.join(__dirname, '..', '..', 'bin', 'ncm-cli.js')
+
+NCMTestRunner.test = tapeCluster(test, NCMTestRunner)
+
+module.exports = NCMTestRunner
+
+function NCMTestRunner (opts) {
+  this.app = express()
+
+  this.httpServer = null
+  this.port = -1
+}
+
+NCMTestRunner.prototype.bootstrap = function bootstrap (cb) {
+  const api = new NCMAPI({
+    packages: mockPackages
+  })
+  const schema = graphql.buildSchema(`
+    scalar JSON
+    type Package {
+      name: String
+      versions: [PackageVersion]
+    }
+    enum Severity {
+      NONE
+      LOW
+      MEDIUM
+      HIGH
+      CRITICAL
+    }
+    type Score {
+      group: String!
+      name: String!
+      pass: Boolean!
+      severity: Severity!
+      title: String!
+      data:  JSON
+    }
+    type PackageVersion {
+      name: String
+      version: String
+      published: Boolean
+      publishedAt: String
+      description: String
+      author: String
+      maintainers: [String!]!
+      keywords: [String!]!
+      scores: [Score!]!
+    }
+    input PackageVersionInput {
+      name: String
+      version: String
+    }
+    type Query {
+      packageVersions(
+        packageVersions: [PackageVersionInput!]!
+      ): [PackageVersion!]!
+      packageVersion(name: String, version: String!): PackageVersion
+    }
+  `)
+
+  this.app.use('*', function (req, res, next) {
+    // console.log('req', {
+    //   method: req.method,
+    //   url: req.originalUrl,
+    //   body: req.body
+    // })
+    next()
+  })
+
+  this.app.use('/', expressGraphql({
+    schema: schema,
+    rootValue: api,
+    graphiql: true
+  }))
+
+  this.httpServer = this.app.listen(0, () => {
+    var addr = this.httpServer.address()
+    this.port = addr.port
+    cb()
+  })
+}
+
+NCMTestRunner.prototype.exec = function _exec (cmd, cb) {
+  let execCmd = 'NCM_TOKEN=token NCM_API=http://localhost:' +
+    this.port + ' node ' + NCM_BIN + ' ' + cmd + ' --color=16m'
+  exec(execCmd, {
+    env: Object.assign({ FORCE_COLOR: 3 }, process.env)
+  }, cb)
+}
+
+NCMTestRunner.prototype.close = function close (cb) {
+  this.httpServer.close(cb)
+}
+
+function NCMAPI (mockData) {
+  this.mockData = mockData
+}
+
+NCMAPI.prototype.packageVersion = function packageVersion (params) {
+  let modules = this.mockData.packages.filter(function findPackage (p) {
+    return p.name === params.name &&
+      p.version === params.version
+  })
+
+  if (modules.length === 0) {
+    return null
+  } else if (modules.length === 1) {
+    return modules[0]
+  } else {
+    throw new Error('duplicate module : ' + params.name +
+      '@' + params.version)
+  }
+}
+
+NCMAPI.prototype.packageVersions = function packageVersions (params) {
+  let versions = params.packageVersions
+
+  let modules = this.mockData.packages.filter(function findPackage (p) {
+    return versions.some(function (v) {
+      return p.name === v.name && p.version === v.version
+    })
+  })
+  return modules
+}
