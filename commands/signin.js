nodesource/ncm-cli

commit 233ebd1f7a34204e3beccaf67d0b1537635b16b4

util: add&use apiRequest, graphql with a refresh+retry
Includes a minor internal change where tokens are pretty mcuh only ever read in the request handler itself.

Fixes: https://github.com/nodesource/ncm-cli/issues/57
PR-URL: https://github.com/nodesource/ncm-cli/pull/64

commands/signin.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const clientRequest = require('../lib/client-request')
 const {
+  apiRequest,
   formatAPIURL,
   queryReadline,
   queryReadlineHidden
 } = require('../lib/util')
-const { getTokens, setTokens } = require('../lib/config')
+const { setTokens } = require('../lib/config')
 const { orgsCli } = require('./orgs')
 const { helpHeader } = require('../lib/help')
 const {
@@ -49,23 +49,19 @@ async function signin (argv, email, password) {
 
   if (SSO) {
     try {
-      const { body: b1 } = await clientRequest({
-        method: 'GET',
-        uri: formatAPIURL('/accounts/auth/social-signin-url', { source: SSO }),
-        json: true
-      })
-      const { url, nonce } = b1
+      const { url, nonce } = await apiRequest(
+        'GET',
+        formatAPIURL('/accounts/auth/social-signin-url', { source: SSO })
+      )
 
       L(line('|➔', 'Open the following very ugly URL:', COLORS.yellow))
       L()
       L(chalk`{${COLORS.blue} ${url}}`)
 
-      const { body: b2 } = await clientRequest({
-        method: 'GET',
-        uri: formatAPIURL('/accounts/auth/retrieve-session', { nonce }),
-        json: true
-      })
-      authData = b2
+      authData = await apiRequest(
+        'GET',
+        formatAPIURL('/accounts/auth/retrieve-session', { nonce })
+      )
     } catch (err) {
       E()
       E(formatError('Failed SSO Authentication.', err))
@@ -90,13 +86,11 @@ async function signin (argv, email, password) {
       const usrInfo = JSON.stringify({ email, password })
 
       try {
-        const { body } = await clientRequest({
-          method: 'POST',
-          uri: formatAPIURL('/accounts/auth/login'),
-          json: true,
-          body: usrInfo
-        })
-        authData = body
+        authData = await apiRequest(
+          'POST',
+          formatAPIURL('/accounts/auth/login'),
+          usrInfo
+        )
       } catch (err) {
         E()
         E(formatError('Failed Authentication.', err))
@@ -105,7 +99,7 @@ async function signin (argv, email, password) {
     }
   }
 
-  if (!authData['session'] || !authData['refreshToken']) {
+  if (!authData.session || !authData.refreshToken) {
     E()
     E(formatError('Bad auth data.', authData))
     E()
@@ -114,22 +108,16 @@ async function signin (argv, email, password) {
   }
 
   setTokens(authData)
-  let { session } = getTokens()
 
   L()
   L(chalk`{${COLORS.light1} Authenticating...}`)
 
   let details
   try {
-    const { body } = await clientRequest({
-      method: 'GET',
-      uri: formatAPIURL('/accounts/user/details'),
-      json: true,
-      headers: {
-        Authorization: `Bearer ${session}`
-      }
-    })
-    details = body
+    details = await apiRequest(
+      'GET',
+      formatAPIURL('/accounts/user/details')
+    )
   } catch (err) {
     E()
     E(formatError('Failed to fetch user info.', err))
@@ -140,7 +128,7 @@ async function signin (argv, email, password) {
 
   L(box('✓', 'Signed in successfully', COLORS.green))
 
-  await orgsCli(session, details)
+  await orgsCli(details)
 }
 
 function printHelp () {

commands/details.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const { getTokens } = require('../lib/config')
 const {
   formatAPIURL,
   graphql
@@ -36,8 +35,6 @@ async function details (argv, arg1, arg2, arg3) {
     return
   }
 
-  const { session } = getTokens()
-
   let name
   let version
   if (!arg1) {
@@ -70,11 +67,6 @@ async function details (argv, arg1, arg2, arg3) {
     return
   }
 
-  const options = {
-    token: session,
-    url: formatAPIURL('/ncm2/api/v2/graphql')
-  }
-
   const vars = {
     name,
     version
@@ -84,7 +76,7 @@ async function details (argv, arg1, arg2, arg3) {
   let data
   try {
     data = await graphql(
-      options,
+      formatAPIURL('/ncm2/api/v2/graphql'),
       `query($name: String!, $version: String!) {
           packageVersion(name: $name, version: $version) {
             name

commands/orgs.js
@@ -1,9 +1,12 @@
 'use strict'
 
-const clientRequest = require('../lib/client-request')
-const { formatAPIURL, queryReadline } = require('../lib/util')
+const {
+  apiRequest,
+  formatAPIURL,
+  queryReadline
+} = require('../lib/util')
 const { helpHeader } = require('../lib/help')
-const { setValue, getValue, getTokens } = require('../lib/config')
+const { setValue, getValue } = require('../lib/config')
 const {
   COLORS,
   header,
@@ -28,19 +31,12 @@ async function orgs (argv, org) {
   L()
   L(header('Select your NodeSource organization'))
 
-  let { session } = getTokens()
-
   let details
   try {
-    const { body } = await clientRequest({
-      method: 'GET',
-      uri: formatAPIURL('/accounts/user/details'),
-      json: true,
-      headers: {
-        Authorization: `Bearer ${session}`
-      }
-    })
-    details = body
+    details = await apiRequest(
+      'GET',
+      formatAPIURL('/accounts/user/details')
+    )
   } catch (err) {
     E()
     E(formatError('Failed to fetch user info.'))
@@ -48,10 +44,10 @@ async function orgs (argv, org) {
     return
   }
 
-  await orgsCli(session, details, org)
+  await orgsCli(details, org)
 }
 
-async function orgsCli (session, details, org) {
+async function orgsCli (details, org) {
   const orgs = [ 'personal' ]
   for (let orgId in details.orgs) {
     orgs.push(details.orgs[orgId].name)

commands/report.js
@@ -1,8 +1,7 @@
 'use strict'
 
 const analyze = require('../lib/ncm-analyze-tree')
-const { getTokens } = require('../lib/config')
-const { formatAPIURL } = require('../lib/util')
+const { formatAPIURL, refreshSession } = require('../lib/util')
 const {
   jsonReport,
   outputReport,
@@ -37,17 +36,18 @@ async function report (argv, _dir) {
     return
   }
 
-  const { session } = getTokens()
-
   /* verify */
   const pkgScores = []
   let hasFailures = false
+
+  // analyze is parallelized between 'pages' of a size...
+  // ... so just always refresh before due to race conditions
+  await refreshSession()
 
   let data
   try {
     data = await analyze({
       dir,
-      token: session,
       url: formatAPIURL('/ncm2/api/v2/graphql')
     })
   } catch (err) {

commands/whitelist.js
@@ -1,15 +1,16 @@
 'use strict'
 
-const { formatAPIURL, refreshSession } = require('../lib/util')
+const {
+  apiRequest,
+  formatAPIURL
+} = require('../lib/util')
 const { helpHeader } = require('../lib/help')
 const { getValue, setValue } = require('../lib/config')
 const whitelistReport = require('../lib/report/whitelist')
 const { reportFailMsg, reportSuccessMsg, SEVERITY_RMAP } = require('../lib/report/util')
-const clientRequest = require('../lib/client-request')
 const {
   COLORS,
   header,
-  line,
   formatError
 } = require('../lib/ncm-style')
 const chalk = require('chalk')
@@ -29,7 +30,6 @@ async function whitelist (argv) {
   }
 
   let { val: policy } = getValue('policyId')
-  let { val: token } = getValue('token')
   let { val: orgId } = getValue('orgId')
   let { val: orgName } = getValue('org')
 
@@ -39,32 +39,19 @@ async function whitelist (argv) {
 
   let policyData
   try {
-    let requestBody = JSON.stringify({
-      query: queries.policy,
-      variables: { orgId }
-    })
-    const { body } = await clientRequest({
-      method: 'POST',
-      uri: formatAPIURL(`/ncm2/api/v2/graphql`),
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      json: true,
-      body: requestBody
-    })
+    const body = await apiRequest(
+      'POST',
+      formatAPIURL(`/ncm2/api/v2/graphql`),
+      {
+        query: queries.policy,
+        variables: { orgId }
+      }
+    )
     policyData = body.data.policies[0]
   } catch (error) {
-    if (error.statusCode === 401) {
-      /* unauthorized -- refresh session */
-      L(line('!', 'Your session has expired.', COLORS.red))
-      await refreshSession()
-      // TODO(mster): retry
-    } else {
-      E()
-      E(formatError('Unable to fetch data.', error))
-      E()
-    }
+    E()
+    E(formatError('Unable to fetch policy data.', error))
+    E()
     process.exitCode = 1
     return
   }
@@ -94,31 +81,18 @@ async function whitelist (argv) {
     }
 
     try {
-      let requestBody = JSON.stringify({
-        query: queries[add ? 'add' : 'remove'],
-        variables: { policy, orgId, entries }
-      })
-      await clientRequest({
-        method: 'POST',
-        uri: formatAPIURL(`/ncm2/api/v2/graphql`),
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        json: true,
-        body: requestBody
-      })
+      await apiRequest(
+        'POST',
+        formatAPIURL(`/ncm2/api/v2/graphql`),
+        {
+          query: queries[add ? 'add' : 'remove'],
+          variables: { policy, orgId, entries }
+        }
+      )
     } catch (error) {
-      if (error.statusCode === 401) {
-        /* unauthorized -- refresh session */
-        L(line('!', 'Your session has expired.', COLORS.red))
-        await refreshSession()
-        // TODO(mster): retry
-      } else {
-        E()
-        E(formatError('Unable to fetch data.', error))
-        E()
-      }
+      E()
+      E(formatError('Unable to modify whitelist data.', error))
+      E()
       process.exitCode = 1
       return
     }
@@ -136,64 +110,38 @@ async function whitelist (argv) {
   async function whitelistList () {
     let whitelistData
     try {
-      let requestBody = JSON.stringify({
-        query: queries.list,
-        variables: { orgId }
-      })
-      const { body } = await clientRequest({
-        method: 'POST',
-        uri: formatAPIURL(`/ncm2/api/v2/graphql`),
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        json: true,
-        body: requestBody
-      })
+      const body = await apiRequest(
+        'POST',
+        formatAPIURL(`/ncm2/api/v2/graphql`),
+        {
+          query: queries.list,
+          variables: { orgId }
+        }
+      )
       whitelistData = body.data.policies[0]
     } catch (error) {
-      if (error.statusCode === 401) {
-        /* unauthorized -- refresh session */
-        L(line('!', 'Your session has expired.', COLORS.red))
-        await refreshSession()
-        // TODO(mster): retry
-      } else {
-        E()
-        E(formatError('Unable to fetch data.', error))
-        E()
-      }
+      E()
+      E(formatError('Unable to fetch policy data.', error))
+      E()
       process.exitCode = 1
       return
     }
 
     let pkgData
     try {
-      let requestBody = JSON.stringify({
-        query: queries.pkgVer,
-        variables: { pkgVers: whitelistData.whitelist }
-      })
-      const { body } = await clientRequest({
-        method: 'POST',
-        uri: formatAPIURL(`/ncm2/api/v2/graphql`),
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        json: true,
-        body: requestBody
-      })
+      const body = await apiRequest(
+        'POST',
+        formatAPIURL(`/ncm2/api/v2/graphql`),
+        {
+          query: queries.pkgVer,
+          variables: { pkgVers: whitelistData.whitelist }
+        }
+      )
       pkgData = body.data
     } catch (error) {
-      if (error.statusCode === 401) {
-        /* unauthorized -- refresh session */
-        L(line('!', 'Your session has expired.', COLORS.red))
-        await refreshSession()
-        // TODO(mster): retry
-      } else {
-        E()
-        E(formatError('Unable to fetch data.', error))
-        E()
-      }
+      E()
+      E(formatError('Unable to fetch whitelist data.', error))
+      E()
       process.exitCode = 1
       return
     }

lib/config.js
@@ -19,6 +19,7 @@ module.exports = {
   setValue: setValue,
   getValue: getValue,
   delValue: delValue,
+  popValue,
   resetState: resetState,
   setTokens: setTokens,
   getTokens: getTokens,
@@ -59,6 +60,19 @@ function delValue (key) {
   }
 
   conf.set(key, keys[key])
+}
+
+// get and delete
+function popValue (key) {
+  validateState()
+
+  if (!keys[key]) {
+    throw new Error(`Attempted to pop invalid config key "${key}"`)
+  }
+
+  const val = conf.get(key)
+  conf.set(key, keys[key])
+  return val
 }
 
 function resetState () {

lib/ncm-analyze-tree.js
@@ -99,7 +99,7 @@ const fetchData = async ({ pkgs, token, url }) => {
     packageVersions: [...pkgs].map(({ name, version }) => ({ name, version }))
   }
 
-  const res = await graphql({ token, url }, query, variables)
+  const res = await graphql(url, query, variables)
   const data = new Set()
   for (const datum of res.packageVersions) {
     // datum.paths = [...pkgs][i].paths

lib/util.js
@@ -5,7 +5,7 @@ const readline = require('readline')
 const url = require('url')
 const pDefer = require('p-defer')
 const clientRequest = require('./client-request')
-const { getTokens, setTokens, api } = require('./config')
+const { setTokens, api, getValue, popValue } = require('./config')
 const { GraphQLClient } = require('graphql-request')
 const {
   formatError
@@ -14,6 +14,7 @@ const {
 const E = console.error
 
 module.exports = {
+  apiRequest,
   formatAPIURL,
   graphql,
   queryReadline,
@@ -30,26 +31,63 @@ function formatAPIURL (pathname, query) {
   })
 }
 
-function graphql (options, query, vars) {
-  if (typeof options === 'string') {
-    options = { options }
-  }
-
-  const client = new GraphQLClient(options.url, {
-    headers: {
-      Authorization: `Bearer ${options.token}`
+async function graphql (uri, query, vars) {
+  let token = getValue('token')
+  let data
+  while (!data && token) {
+    try {
+      const client = new GraphQLClient(uri, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      data = await client.request(query, vars)
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        token = await refreshSession()
+      } else {
+        throw error
+      }
     }
-  })
+  }
+  return data
+}
 
-  return client.request(query, vars)
+async function apiRequest (method, uri, reqbody) {
+  let token = getValue('token')
+  let body
+  while (!body && token) {
+    try {
+      const response = await clientRequest({
+        method,
+        uri,
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        json: true,
+        body: JSON.stringify(reqbody)
+      })
+      body = response.body
+    } catch (error) {
+      if (error.statusCode === 401) {
+        token = await refreshSession()
+      } else {
+        throw error
+      }
+    }
+  }
+  return body
 }
 
 async function refreshSession () {
-  let { refreshToken } = getTokens()
+  const refreshToken = popValue('refreshToken')
+  if (!refreshToken || !refreshToken.trim()) {
+    return
+  }
 
   let refreshData
   try {
-    const { body } = await clientRequest({
+    const response = await clientRequest({
       method: 'GET',
       uri: formatAPIURL('/accounts/auth/refresh'),
       json: true,
@@ -57,7 +95,7 @@ async function refreshSession () {
         Authorization: `Bearer ${refreshToken}`
       }
     })
-    refreshData = body
+    refreshData = response.body
   } catch (err) {
     E()
     E(formatError('Failed to refresh session.', err))
@@ -67,6 +105,7 @@ async function refreshSession () {
   }
 
   setTokens(refreshData)
+  return refreshData.session
 }
 
 function queryReadline (query) {

commit 9958e40e152e6ab5d946c016ab1fc6d6f1faaba7

signin: fix email signin
Changes the signin logic back to using clientRequest() rather than apiRequest().

Turns out this broke in
233ebd1f7a34204e3beccaf67d0b1537635b16b4
for some obscure reason related to the apiRequest() logic,
which I was unable to fully figure out.

PR-URL: https://github.com/nodesource/ncm-cli/pull/109

commands/signin.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const clientRequest = require('../lib/client-request')
 const {
   apiRequest,
   formatAPIURL,
@@ -49,19 +50,23 @@ async function signin (argv, email, password) {
 
   if (SSO) {
     try {
-      const { url, nonce } = await apiRequest(
-        'GET',
-        formatAPIURL('/accounts/auth/social-signin-url', { source: SSO })
-      )
+      const { body: b1 } = await clientRequest({
+        method: 'GET',
+        uri: formatAPIURL('/accounts/auth/social-signin-url', { source: SSO }),
+        json: true
+      })
+      const { url, nonce } = b1
 
       L(line('|➔', 'Open the following very ugly URL:', COLORS.yellow))
       L()
       L(chalk`{${COLORS.blue} ${url}}`)
 
-      authData = await apiRequest(
-        'GET',
-        formatAPIURL('/accounts/auth/retrieve-session', { nonce })
-      )
+      const { body: b2 } = await clientRequest({
+        method: 'GET',
+        uri: formatAPIURL('/accounts/auth/retrieve-session', { nonce }),
+        json: true
+      })
+      authData = b2
     } catch (err) {
       E()
       E(formatError('Failed SSO Authentication.', err))
@@ -86,11 +91,13 @@ async function signin (argv, email, password) {
       const usrInfo = JSON.stringify({ email, password })
 
       try {
-        authData = await apiRequest(
-          'POST',
-          formatAPIURL('/accounts/auth/login'),
-          usrInfo
-        )
+        const { body } = await clientRequest({
+          method: 'POST',
+          uri: formatAPIURL('/accounts/auth/login'),
+          json: true,
+          body: usrInfo
+        })
+        authData = body
       } catch (err) {
         E()
         E(formatError('Failed Authentication.', err))