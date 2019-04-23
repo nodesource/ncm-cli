1fefe4821c1efc39bacbc766db157ec674380153
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2019-04-23 16:42:26 -0700

    tests: do a comparison on stderr
    This allows the output to be printed in case of a failure, rather than doing a boolean coercion.
    
    PR-URL: https://github.com/nodesource/ncm-cli/pull/140

--- test/report.js
+++ test/report.js
@@ -6,7 +6,7 @@ const MOCK_PROJECT = '--dir=./test/fixtures/mock-project'
 TestRunner.test('report output matches snapshot', (runner, t) =>
   runner.exec(`report ${MOCK_PROJECT}`, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output')
     t.ok(/mock-project Report/.test(stdout))
     t.ok(/36 .+packages checked/.test(stdout))
@@ -24,7 +24,7 @@ TestRunner.test('report --compliance output', (runner, t) =>
   runner.exec(`report ${MOCK_PROJECT} --compliance`,
     (err, stdout, stderr) => {
       t.equal(err.code, 1)
-      t.notOk(stderr)
+      t.equal(stderr, '')
       t.matchSnapshot(stdout, 'report-output-compliance')
 
       const out = stdout.toString()
@@ -41,7 +41,7 @@ TestRunner.test('report --compliance output', (runner, t) =>
 TestRunner.test('report -c output', (runner, t) =>
   runner.exec(`report ${MOCK_PROJECT} -c`, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-compliance')
 
     const out = stdout.toString()
@@ -57,7 +57,7 @@ TestRunner.test('report --filter=compliance output', (runner, t) => {
   const cmd = `report ${MOCK_PROJECT} --filter=compliance`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-compliance')
 
     const out = stdout.toString()
@@ -74,7 +74,7 @@ TestRunner.test('report --filter=compliance output', (runner, t) => {
 TestRunner.test('report --security output', (runner, t) =>
   runner.exec(`report ${MOCK_PROJECT} --security`, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-security')
 
     const out = stdout.toString()
@@ -94,7 +94,7 @@ TestRunner.test('report --security output', (runner, t) =>
 TestRunner.test('report -s output', (runner, t) =>
   runner.exec(`report ${MOCK_PROJECT} -s`, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-security')
 
     const out = stdout.toString()
@@ -110,7 +110,7 @@ TestRunner.test('report --filter=security output', (runner, t) =>
   runner.exec(`report ${MOCK_PROJECT} --filter=security`,
     (err, stdout, stderr) => {
       t.equal(err.code, 1)
-      t.notOk(stderr)
+      t.equal(stderr, '')
       t.matchSnapshot(stdout, 'report-output-security')
 
       const out = stdout.toString()
@@ -131,7 +131,7 @@ TestRunner.test('report --filter=high --security output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=high --security`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-high-security')
 
     const out = stdout.toString()
@@ -152,7 +152,7 @@ TestRunner.test('report --filter=high output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=high`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-high-security')
 
     const out = stdout.toString()
@@ -173,7 +173,7 @@ TestRunner.test('report --filter=h output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=h --color=16m`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-high-security')
 
     const out = stdout.toString()
@@ -194,7 +194,7 @@ TestRunner.test('report --filter=high,security output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=high,security`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-high-security')
 
     const out = stdout.toString()
@@ -215,7 +215,7 @@ TestRunner.test('report --filter=medium --security output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=medium --security`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-med-security')
 
     const out = stdout.toString()
@@ -236,7 +236,7 @@ TestRunner.test('report --filter=m --security output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=m --security`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-med-security')
 
     const out = stdout.toString()
@@ -257,7 +257,7 @@ TestRunner.test('report --filter=low --security output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=low --security`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-med-security')
 
     const out = stdout.toString()
@@ -278,7 +278,7 @@ TestRunner.test('report --filter=l --security output', (runner, t) => {
   let cmd = `report ${MOCK_PROJECT} --filter=l --security`
   runner.exec(cmd, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'report-output-med-security')
 
     const out = stdout.toString()
@@ -298,7 +298,7 @@ TestRunner.test('report --filter=l --security output', (runner, t) => {
 TestRunner.test('report output matches snapshot', (runner, t) =>
   runner.exec(`report --long ${MOCK_PROJECT}`, (err, stdout, stderr) => {
     t.equal(err.code, 1)
-    t.notOk(stderr)
+    t.equal(stderr, '')
     t.matchSnapshot(stdout, 'long-report-output')
     t.ok(/mock-project Report/.test(stdout))
     t.ok(/has-flag @ 3.0.0/.test(stdout))
