86220c3d5d48b479d07397adc7f9e1fc86618ea9
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2019-04-23 16:28:48 -0700

    tests: add tests for `ncm install`
    Probably should have been done in
    https://github.com/nodesource/ncm-cli/pull/71
    
    Closes: https://github.com/nodesource/ncm-cli/issues/138
    
    PR-URL:  https://github.com/nodesource/ncm-cli/pull/139

---
+++ test/install.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const NCMTestRunner = require('./lib/test-runner.js')
+const path = require('path')
+
+const XDG_CONFIG_HOME = path.join(__dirname, '.tmp')
+
+NCMTestRunner.test('install output matches snapshot', async (runner, t) => {
+  {
+    const MOCK_INSTALL_BIN = path.join('test', 'lib', 'install-mock-bin.js')
+
+    const { stdout, stderr } = await runner.execP(
+      `config set installBin ${MOCK_INSTALL_BIN}`,
+      { XDG_CONFIG_HOME }
+    )
+
+    t.equal(stderr, '')
+    t.matchSnapshot(stdout, 'config-set-output')
+
+    const out = stdout.toString()
+    t.match(out, /<installBin> has been set to:\n.+install-mock-bin\.js/)
+  }
+
+  {
+    const { stdout, stderr } = await runner.execP(
+      'install npm @ 6.8.0 --force',
+      { XDG_CONFIG_HOME }
+    )
+
+    t.equal(stderr, '')
+    t.matchSnapshot(stdout, 'install-output')
+
+    const out = stdout.toString()
+    t.match(out, 'SUBCOMMAND ARGS: [ \'install\', \'npm@6.8.0\', \'--force\'')
+  }
+})
