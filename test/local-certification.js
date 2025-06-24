8c80f5030cca077466e1f7496a66904ebfd3c63b
Minwoo Jung <nodecorelab@gmail.com>
2025-06-24 18:24:39 +0900

    ++ b/test/local-certification.js

--- test/local-certification.js
+++ test/local-certification.js
@@ -11,7 +11,7 @@ const TEST_PROJECT_DIR = path.join(os.tmpdir(), 'ncm-cli-local-cert-test-' + Dat
 const MOCK_PROJECT = `--dir=${TEST_PROJECT_DIR}`
 
 // Function to create a test package.json
-function setupTestProject() {
+function setupTestProject () {
   // Create test directory if it doesn't exist
   if (!fs.existsSync(TEST_PROJECT_DIR)) {
     fs.mkdirSync(TEST_PROJECT_DIR, { recursive: true })
@@ -23,9 +23,9 @@ function setupTestProject() {
     version: '1.0.0',
     description: 'Test project for local certification',
     dependencies: {
-      'lodash': '^4.17.21',
-      'express': '^4.18.2',
-      'chalk': '^4.1.2'
+      lodash: '^4.17.21',
+      express: '^4.18.2',
+      chalk: '^4.1.2'
     }
   }
 
@@ -58,8 +58,8 @@ function setupTestProject() {
 
   // Create .ncm-whitelist.json for local testing
   const whitelist = {
-    "packages": [
-      { "name": "lodash", "version": "4.17.21" }
+    packages: [
+      { name: 'lodash', version: '4.17.21' }
     ]
   }
 
@@ -72,10 +72,10 @@ function setupTestProject() {
 }
 
 // Clean up test project
-function cleanupTestProject() {
+function cleanupTestProject () {
   if (fs.existsSync(TEST_PROJECT_DIR)) {
     // Recursive delete function
-    const deleteFolderRecursive = function(dir) {
+    const deleteFolderRecursive = function (dir) {
       if (fs.existsSync(dir)) {
         fs.readdirSync(dir).forEach((file) => {
           const curPath = path.join(dir, file)
@@ -88,7 +88,7 @@ function cleanupTestProject() {
         fs.rmdirSync(dir)
       }
     }
-    
+
     try {
       deleteFolderRecursive(TEST_PROJECT_DIR)
     } catch (err) {
@@ -107,21 +107,21 @@ NCMTestRunner.createTest('local certification mode with ncm-ng adapter', (runner
       // We expect the command to run successfully with local certification
       t.is(err, null, 'Command should complete successfully')
       t.is(stderr, '', 'Should not have stderr output')
-      
+
       // Check for indicators of local certification
       t.regex(stdout, /ncm-cli-local-cert-test.*Report/, 'Should show the proper report title')
       t.regex(stdout, /packages checked/, 'Should check packages')
-      
+
       // Check for MIT license indicator instead of specific checkmark character
       t.regex(stdout, /MIT/, 'MIT license should be present')
-      
+
       // Check that risk level is displayed properly
       t.regex(stdout, /Low/, 'Risk level should be displayed')
-      
+
       // Check that lodash is present in the report
       t.regex(stdout, /lodash/, 'Should include lodash in report')
       t.regex(stdout, /4\.17\.21/, 'Should include lodash version')
-      
+
       resolve()
     })
   })
@@ -140,13 +140,13 @@ NCMTestRunner.createTest('local certification respects local whitelist', (runner
     runner.exec(`report ${MOCK_PROJECT} -l`, (err, stdout, stderr) => {
       t.is(err, null, 'Command should complete successfully')
       t.is(stderr, '', 'Should not have stderr output')
-      
+
       // Local mode should handle whitelisted packages properly
       t.regex(stdout, /lodash @ 4\.17\.21/, 'Should include whitelisted lodash in report')
-      
+
       // Check compliance information is correct
       t.regex(stdout, /All modules compliant/, 'Should show all modules as compliant')
-      
+
       resolve()
     })
   })
@@ -157,13 +157,13 @@ NCMTestRunner.createTest('license badge display for MIT licenses', (runner, t) =
   return new Promise(/** @type {(resolve: any) => void} */ (resolve) => {
     runner.exec(`report ${MOCK_PROJECT} -l`, (err, stdout, stderr) => {
       t.is(err, null, 'Command should complete successfully')
-      
+
       // Check for MIT licenses
       t.regex(stdout, /MIT/, 'MIT license should be present')
-      
+
       // Make sure 'UNKNOWN' is not present for MIT licenses
       t.notRegex(stdout, /UNKNOWN/, 'Should not show UNKNOWN for MIT licenses')
-      
+
       resolve()
     })
   })
@@ -174,13 +174,13 @@ NCMTestRunner.createTest('risk badge display in report', (runner, t) => {
   return new Promise(/** @type {(resolve: any) => void} */ (resolve) => {
     runner.exec(`report ${MOCK_PROJECT} -l`, (err, stdout, stderr) => {
       t.is(err, null, 'Command should complete successfully')
-      
+
       // Risk level should be displayed
       t.regex(stdout, /Low/, 'Risk level should be displayed')
-      
+
       // Should not show "None" risk level
       t.notRegex(stdout, /None/, 'Should not display "None" as a risk level')
-      
+
       resolve()
     })
   })
